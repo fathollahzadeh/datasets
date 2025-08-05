b8f473e619653ff06496ee8e7f21ecd8fc1d91f4
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2025-08-05 11:48:14 -0400

    minor

--- PublicBIbenchmark/refine.py
+++ PublicBIbenchmark/refine.py
@@ -39,26 +39,26 @@ if __name__ == '__main__':
     output_file = f'{input_file}_clean'
     expected_cols = {'HashTags': 101, 'Corporations': 27, 'Euro2016': 11, 'IGlocations2': 20,'Rentabilidad': 0,'Romance': 12,'TableroSistemaPenal': 0, 'NYC': 54}
     if args.dataset_name == "Rentabilidad":
-        if "rentabilidad2.table" in input_file:
+        if "Rentabilidad_2.csv" in input_file:
             expected_columns = 138
         else :
             expected_columns = 141
     elif args.dataset_name == "TableroSistemaPenal":
-        if "tablerosistemapenal1.table" in input_file:
+        if "TableroSistemaPenal_1.csv" in input_file:
             expected_columns = 27
-        elif "tablerosistemapenal2.table" in input_file:
+        elif "TableroSistemaPenal_2.csv" in input_file:
             expected_columns = 22
-        elif "tablerosistemapenal3.table" in input_file:
+        elif "TableroSistemaPenal_3.csv" in input_file:
             expected_columns = 21
-        elif "tablerosistemapenal4.table" in input_file:
+        elif "TableroSistemaPenal_4.csv" in input_file:
             expected_columns = 21
-        elif "tablerosistemapenal5.table" in input_file:
+        elif "TableroSistemaPenal_5.csv" in input_file:
             expected_columns = 21
-        elif "tablerosistemapenal6.table" in input_file:
+        elif "TableroSistemaPenal_6.csv" in input_file:
             expected_columns = 13
-        elif "tablerosistemapenal7.table" in input_file:
+        elif "TableroSistemaPenal_7.csv" in input_file:
             expected_columns = 27
-        elif "tablerosistemapenal8.table" in input_file:
+        elif "TableroSistemaPenal_8.csv" in input_file:
             expected_columns = 22            
     else:
          expected_columns = expected_cols[args.dataset_name]
@@ -79,7 +79,7 @@ if __name__ == '__main__':
                 cleaned = [clean_field(f, i) for i, f in enumerate(row)]
 
                 if len(cleaned) != expected_columns:
-                    print(f"Line {line_number}: Skipped (got {len(cleaned)} columns)")
+                    print(f"Line {line_number}: Skipped (got {len(cleaned)} columns)")                    
                     continue
 
                 writer.writerow(cleaned)
